93121939b485b8acd2ee185c4af82bea8ba8a0f3
legen <504883942@qq.com>
2017-03-29 17:11:00 +0800

    - 可以执行各种命令行里能执行的程序，除了自己写的exe文件以外，包括ping ,使用python,使用nodejs等等，扩展性非常好 - 现在可以添加只执行一次的任务 - 优化了日志文件的操作，添加了自定义引索。从字节流获取引索，对于非常大的日志文件(约227 MB)搜索引索，只需要2秒左右。通常情况下每天的日志约在5MB以下。如果需要指定读取日志文件中的某一条记录效率会非常高。

--- TimeSystem/ModelExtension.cs
+++ TimeSystem/ModelExtension.cs
@@ -13,7 +13,7 @@ namespace TimeSystem
                 TaskEntities db = new TaskEntities();
                 return db.Application_t.FirstOrDefault(p=>p.Uid==this.ApplicationUid);
             }
-        }
+        }  
         public string realLogPath {
             get {
                 var path = this.LogPath + this.Id + "-" + this.Uid.ToString() + @"\";
@@ -21,9 +21,55 @@ namespace TimeSystem
                 return path;
             }
         }
+        public List<long> GetIndex(string filename) {
+            FileStream fs;
+            //获得文件所在路径  
+ 
+            try
+            {
+                fs = new FileStream(realLogPath + filename, FileMode.Open);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            List<long> index = new List<long>();
+            int c;
+            while ((c = fs.ReadByte()) != -1)
+            {
+                if (c == 36)
+                {
+                    index.Add(fs.Position - 1);
+                }
+            }
+            fs.Close();
+            return index;
+        }
+        public List<string> GetLogItems(string filename,int[] list ) {
+           
+            List<long> index = GetIndex(filename);
+            FileStream fs  = new FileStream(realLogPath + filename, FileMode.Open);
+            List<string> items = new List<string>();
+            //获得文件所在路径  
+            foreach(var i in list) {
+                items.Add(GetLogItem((int)index[i-1], (int)index[i],fs));
+            }
+            return items;
+        }
+        private string GetLogItem(int start,int end, FileStream fs) {
+            if (start <= 0)
+            {
+                return "";
+            }
+            fs.Position = start;
+            long length = end-start;
+            byte[] bytes = new byte[length];
+            fs.Read(bytes, 0, (int)length);
+            return Encoding.Default.GetString(bytes);
+        }
         public string[] logs {
             get {
-               return Directory.GetFiles(realLogPath, "*.txt");
+               return Directory.GetFiles(realLogPath, "*.txt").Select(p=>Path.GetFileName(p)).ToArray();
             }
         }
     }
--- TimeSystem/TaskController.cs
+++ TimeSystem/TaskController.cs
@@ -18,37 +18,54 @@ namespace TimeSystem
         [HttpGet]
         public IHttpActionResult All()
         {
-            var jobs = TaskHelper.Schedules.Select(p =>new {
-                Id=p.Id,
-                Application=p.application.Name,
-                Description=p.application.Description,
-                Memo=p.Memo,
-                Cron=p.Cron,
-                Enable=p.Enable,
-                Logfile=p.logs
+            var jobs = TaskHelper.Schedules.Select(p => new
+            {
+                Id = p.Id,
+                Application = p.application.Name,
+                Description = p.application.Description,
+                Memo = p.Memo,
+                Cron = p.Cron,
+                Enable = p.Enable,
+                Logfile = p.logs
             }).ToArray();
             return Json<dynamic>(jobs);
         }
         [HttpGet]
-        public IHttpActionResult logfile(int id,string filename) {
-            var job = TaskHelper.Schedules.First(p => p.Id == id);
-            lock (job)
+        public IHttpActionResult logfile(int taskid, string filename,int[] index)
+        {
+            var job = TaskHelper.Schedules.First(p => p.Id == taskid);
+            if (index!=null)
             {
-                StreamReader sr = new StreamReader(job.realLogPath + filename, Encoding.Default);
-                List<string> logs = new List<string>();
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    logs.Add(line);
-                }
-                sr.Close();
-                return Json(logs)  ;
+                return Json(job.GetLogItems(filename, index) );
             }
-         
+            else {
+                return Json<dynamic>(new {
+                    count=job.GetIndex(filename)
+                });
+            }
+
+
+
+            //lock (job)
+            //{
+
+
+            //    StreamReader sr = new StreamReader(job.realLogPath + filename, Encoding.Default);
+            //    List<string> logs = new List<string>();
+            //    string line;
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        logs.Add(line);
+            //    }
+            //    sr.Close();
+            //    return Json(logs);
+            //}
+
         }
 
         [HttpGet]
-        public bool refresh() {
+        public bool refresh()
+        {
             try
             {
 
@@ -57,8 +74,9 @@ namespace TimeSystem
 
                 return true;
             }
-            catch (Exception ex){
-                LogHelper.WriteLog("刷新失败"+ ex);
+            catch (Exception ex)
+            {
+                LogHelper.WriteLog("刷新失败" + ex);
 
                 return false;
             }
