87f992959ae857fe457b159f0a6417ea1c3d1a9c
legen <504883942@qq.com>
2017-05-12 19:34:29 +0800

    i

--- TimeSystem/FullTask.cs
+++ TimeSystem/FullTask.cs
@@ -18,14 +18,14 @@ namespace TimeSystem
         /// <param name="context"></param>
         public void Execute(IJobExecutionContext context)
         {
-          
+
             try
             {
                 var job = context.JobDetail.JobDataMap.Get("Schedule") as Schedule_t;
                 LogHelper.WriteLog(job, "开始执行");
 
                 ExeJob(job);
-                LogHelper.WriteLog(job,"执行成功");
+                LogHelper.WriteLog(job, "执行成功");
             }
             catch (Exception ex)
             {
@@ -40,44 +40,50 @@ namespace TimeSystem
         {
 
             //处理调用时的参数,默认没有参数
-            Type[] types = new Type[] { };
-            object[] paras = new object[] { };
-            string[] parastring = new string[] { };
-            if (job.Paras != null && job.Paras.Split(',').Length > 0)
-            {
-                parastring = job.Paras.Split(',');
-                //有参数情况,参数默认是一个字符串数组
-                types = new Type[] { typeof(string[]) };
-                paras = new object[1];
-                paras[0] = new string[parastring.Length];
-                for (int i = 0; i < parastring.Length; i++)
-                {
-                    (paras[0] as string[])[i] = parastring[i];
-                }
-            }
+            //Type[] types = new Type[] { };
+            //object[] paras = new object[] { };
+            //string[] parastring = new string[] { };
+            //if (job.Paras != null && job.Paras.Split(',').Length > 0)
+            //{
+            //    parastring = job.Paras.Split(',');
+            //    //有参数情况,参数默认是一个字符串数组
+            //    types = new Type[] { typeof(string[]) };
+            //    paras = new object[1];
+            //    paras[0] = new string[parastring.Length];
+            //    for (int i = 0; i < parastring.Length; i++)
+            //    {
+            //        (paras[0] as string[])[i] = parastring[i];
+            //    }
+            //}
 
 
             Process proc = null;
-            try
-            {
-                proc = new Process();
-                proc.StartInfo.FileName = "cmd.exe";
-                proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(job.application.Path);
-                proc.StartInfo.RedirectStandardOutput = true;
-                proc.StartInfo.RedirectStandardInput = true;
-                proc.StartInfo.UseShellExecute = false;
-                proc.StartInfo.CreateNoWindow = true;
-                proc.Start();
-                string appcmd = job.application.Path + " " + job.Paras + " >> " + job.realLogPath + DateTime.Now.ToString(job.LogPattern)+".txt";
-                proc.StandardInput.WriteLine(appcmd);
-                proc.StandardInput.WriteLine("exit");
-                proc.WaitForExit();
-                proc.Close();
-            }
-            catch (Exception ex)
-            {
-                LogHelper.WriteLog(job, ex.ToString());
-            }
+
+            proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(job.application.Path);
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardInput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append(job.application.Path);
+            //sb.Append(' ');
+            //sb.Append(@job.Paras);
+            //sb.Append(" >> ");
+            //sb.Append(job.realLogPath);
+            //sb.Append(DateTime.Now.ToString(job.LogPattern));
+            //sb.Append(".txt");
+            //sb.ToString();
+
+             string appcmd = job.application.Path + " " + job.Paras + " >> " + job.realLogPath + DateTime.Now.ToString(job.LogPattern)+".txt";
+
+            //var ar = sb.ToString().ToArray();
+            proc.StandardInput.WriteLine(appcmd);
+            proc.StandardInput.WriteLine("exit");
+            proc.WaitForExit();
+            proc.Close();
         }
     }
 }
