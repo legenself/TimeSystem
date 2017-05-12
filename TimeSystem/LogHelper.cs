24c67ec65ba6663124ba665076f8a757870c2ab3
legen <504883942@qq.com>
2017-05-12 19:41:40 +0800

    Revert "Revert "取消了向服务器的日志，感觉价值不大..""
    This reverts commit 69e04ac6616cca9b9bd4ba3a2ba2c30b2bc19e14.

--- TimeSystem/LogHelper.cs
+++ TimeSystem/LogHelper.cs
@@ -14,20 +14,20 @@ namespace TimeSystem
         /// <param name="task"></param>
         public static void WriteLog(Schedule_t task, string msg = null)
         {
-            lock (typeof(LogHelper))
-            {
-                TaskEntities db = new TaskEntities();
-                db.Log_t.Add(new Log_t()
-                {
-                    Message = msg,
-                    Result = 0,
-                    Time = DateTime.Now,
-                    ScheduleUid = task.Uid,
-                    Type = 0
-                });
-                db.SaveChanges();
+            //lock (typeof(LogHelper))
+            //{
+            //    TaskEntities db = new TaskEntities();
+            //    db.Log_t.Add(new Log_t()
+            //    {
+            //        Message = msg,
+            //        Result = 0,
+            //        Time = DateTime.Now,
+            //        ScheduleUid = task.Uid,
+            //        Type = 0
+            //    });
+            //    db.SaveChanges();
 
-            }
+            //}
         }
         /// <summary>
         /// 输出日志到数据库
@@ -35,18 +35,18 @@ namespace TimeSystem
         /// <param name="task"></param>
         public static void WriteLog(string msg)
         {
-            lock (typeof(LogHelper))
-            {
-                TaskEntities db = new TaskEntities();
-                db.Log_t.Add(new Log_t()
-                {
-                    Message = msg,
-                    Result = 0,
-                    Time = DateTime.Now,
-                    Type = 0
-                });
-                db.SaveChanges();
-            }
+            //lock (typeof(LogHelper))
+            //{
+            //    TaskEntities db = new TaskEntities();
+            //    db.Log_t.Add(new Log_t()
+            //    {
+            //        Message = msg,
+            //        Result = 0,
+            //        Time = DateTime.Now,
+            //        Type = 0
+            //    });
+            //    db.SaveChanges();
+            //}
         }
     }
 }
