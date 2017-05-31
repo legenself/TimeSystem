75847d83044504ffb0c423fbc0374577ef041ab5
legen <504883942@qq.com>
2017-05-31 11:13:20 +0800

    nodejs访问redis 感觉现在还不是很适应，在考虑要不要回归C#

---
+++ Taskmaster/Rules/errorpercentrule.js
@@ -0,0 +1,24 @@
+﻿var os = require('os');
+var redis = require("redis"),
+    RDS_PORT = 6379,
+    RDS_HOST = "192.168.1.70",
+    RDS_PWD = '',
+    PDS_OPTS = { auth_pass: RDS_PWD };
+
+
+
+
+rule = new Promise(function (resolve, reject) {
+    var client = redis.createClient(RDS_PORT, RDS_HOST);
+    client.keys("status_*", function (err, res) {
+        console.log(res);
+        resolve({ type: false, msg: "内存警告" });
+        client.end(true);
+    });
+
+ 
+
+
+})
+
+exports.rule = rule;
